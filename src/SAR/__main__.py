8e06952c2b7c063bf96380900e94b99e00073ded
Romolo Politi <romolo.politi@inaf.it>
2024-01-16 14:52:42 +0100

    fix bug

--- src/SAR/__main__.py
+++ src/SAR/__main__.py
@@ -185,7 +185,7 @@ def action(kernel_folder: Path, debug: bool, verbose: int, save_current: bool,sa
         save_kernel(kernels)
         txt=f"The SOIM Output was updatet due a Metakernel changes ({Path(info['latest']).name})"
         project_list_file = Path('~/projects/project_list.yml').expanduser()
-        core_soim(read_yaml(project_list_file),info['latest'],kernel_folder)
+        core_soim(read_yaml(project_list_file),info['latest'],kernel_folder,Path('~/output_soim').expanduser())
         try:
             mail('SOIM Output Updated', text=txt, html=page(
                 f"<strong>{txt}</strong><br/>"))
